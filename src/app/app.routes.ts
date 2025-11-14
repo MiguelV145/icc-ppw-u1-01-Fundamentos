5f8626d95b91a1074c0ece253cc54bd25c7b50d4
miguelv145 <miguelvanegas145@gmail.com>
2025-11-14 17:45:32 -0500

    04-formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomePages } from './features/homePages/homePages';
 import { PerfilPages as PefilPage } from './features/perfilPages/perfilPages';
 import { ProyectoPage } from './features/ProyectoPage/ProyectoPage';
 import { ProyectosPages2 } from './features/ProyectosPages2/ProyectosPages2';
+import { Formulario } from './features/Formulario/Formulario/Formulario';
 
 
 export const routes: Routes = [
@@ -26,6 +27,11 @@ export const routes: Routes = [
         {
             path: 'proyectoPage2',
             component: ProyectosPages2
+        },
+
+        {
+            path: 'formulario',
+            component:Formulario
         }
 
 ];
